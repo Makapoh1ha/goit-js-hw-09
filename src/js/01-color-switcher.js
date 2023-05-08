1c5ed733e59548c184da6b2dda4881e60ceb9158
Makapoh1ha <makapohiha@gmail.com>
2023-05-08 17:44:09 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,10 +2,10 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
-let timerId = null;
+let timerId = 0;
 const INTERVAL_DURATION = 1000;
-startBtn.disabled = false;
-stopBtn.disabled = true;
+let timerActive = false;
+
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -15,22 +15,26 @@ function updateBodyBgColor (){
     body.style.backgroundColor =  getRandomHexColor();
 }  
 
-startBtn.addEventListener('click', onStartBtnClick);
+startBtn.addEventListener('click', () => { 
+  if (timerActive) {
+    return;
+  }
+  onStartBtnClick();
+});
 
 function onStartBtnClick(){
-    console.log(`Call onStartBtnClick every ${INTERVAL_DURATION} ms`)
       timerId = setInterval(updateBodyBgColor, INTERVAL_DURATION);
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
+  timerActive = true;
 };
 
 stopBtn.addEventListener('click', onStopBtnClick);
 
 function onStopBtnClick(){
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-    clearInterval(timerId);
-    console.log(`Interval has stopped!`);
+    
+  clearInterval(timerId);
+  timerActive = false;
+  console.log(`Interval has stopped!`);
+  
 };
 
 
