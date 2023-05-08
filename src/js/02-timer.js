c56c4384544b8571d64b87633a94bc827f308258
Makapoh1ha <makapohiha@gmail.com>
2023-05-08 17:49:21 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -42,14 +42,14 @@ const options = {
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-        if(selectedDates[0] < Date.now()) {
+    onClose({ selectedDates }) {
+        if(selectedDates < Date.now()) {
             Notify.failure('Please choose a date in the future');
             userDate = new Date();
         } else { 
             buttonEl.disabled = false;
             buttonEl.classList.remove('disabled');
-            userDate = selectedDates[0];
+            userDate = selectedDates;
         }
     },
 };
